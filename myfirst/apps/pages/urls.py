6496d68860dc0446de618c8d7996bf7861d934d0
IllKA8 <paklinvir@gmail.com>
2024-06-23 18:04:11 +0300

    add button and review code

--- myfirst/apps/pages/urls.py
+++ myfirst/apps/pages/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     path('article1_FrendlyThug52NGG', views.article1, name='article1'),
-    path('article2_Baskov', views.article2, name='article2')
+    path('article2_Baskov', views.article2, name='article2'),
+    path('article3_interesting', views.article3, name='article3'),
+    path('article4_questions_and_answers', views.article4, name='article4'),
+    path('article_main', views.article5, name='article5')
 ]
--- myfirst/apps/pages/views.py
+++ myfirst/apps/pages/views.py
@@ -10,3 +10,13 @@ def article1(request):
 
 def article2(request):
     return render(request, 'article/article2_Baskov.html')
+
+def article3(request):
+    return render(request, 'article/article3_interesting.html')
+
+def article4(request):
+    return render(request, 'article/article4_questions_and_answers.html')
+
+def article5(request):
+    return render(request, 'article/article_main.html')
+
